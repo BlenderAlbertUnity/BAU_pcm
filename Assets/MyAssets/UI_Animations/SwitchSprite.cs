1a19071e3ed35377c00434d0e8becd4019d5c47d
Einstein Games <einsteingames@Einstein-no-Mac-mini.local>
2018-01-18 12:52:31 +0900

    add script

---
+++ Assets/MyAssets/UI_Animations/SwitchSprite.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSprite : MonoBehaviour {
+
+	public GameObject[] sprites;
+	public float interval = 1.0f;
+	float curTime;
+	int index = 0;
+
+	// Use this for initialization
+	void Start () {
+		if (sprites.Length < 1)
+			this.enabled = false;
+
+		for (int i = 0; i < sprites.Length; i++) {
+			if (i == index) {
+				sprites [i].SetActive (true);
+			} else {
+				sprites [i].SetActive (false);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		curTime += Time.deltaTime;
+
+		if (curTime > interval) {
+			curTime -= interval;
+
+			index++;
+
+			if (index > sprites.Length - 1)
+				index = 0;
+
+			for (int i = 0; i < sprites.Length; i++) {
+				if (i == index) {
+					sprites [i].SetActive (true);
+				} else {
+					sprites [i].SetActive (false);
+				}
+			}
+		}
+			
+	}
+}
